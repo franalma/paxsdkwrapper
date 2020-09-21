8868f6ce67c2f14518b31910c6e62561af435d23
franalmeida@gmail.com <46890781+franalma@users.noreply.github.com>
2020-09-21 07:16:56 +0200

    Update Log.java

--- src/main/java/com/diusframi/tele/logs/Log.java
+++ src/main/java/com/diusframi/tele/logs/Log.java
@@ -2,15 +2,54 @@ package com.diusframi.tele.logs;
 
 import android.content.Context;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
 public class Log {
     com.diusframi.android.logs2json.Log logger;
     private static Log instance;
     private String tag = "PaxSdkWrapper";
     private static Context context;
+    private final static String FILE_NAME = "sdkwrapper.json";
 
     private Log(){
-        logger = new com.diusframi.android.logs2json.Log(tag, "sdkwrapper.json");
+        logger = new com.diusframi.android.logs2json.Log(tag, FILE_NAME);
         logger.setToFile(true);
+    }
+
+    public String getLogFullInfo() {
+        JSONArray jac = new JSONArray();
+        try{
+            File dir = context.getFilesDir();
+            BufferedReader reader = new BufferedReader(new FileReader(dir+"/"+ FILE_NAME));
+            String line;
+            while((line = reader.readLine()) != null){
+                line = line.replaceAll("\\\\","");
+                JSONObject joc = new JSONObject(line);
+                jac.put(joc);
+            }
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return jac.toString();
+    }
+    public boolean clearLogs(){
+        File file = new File (context.getFilesDir()+"/"+FILE_NAME);
+        boolean res = true;
+        try{
+            res = file.delete();
+            res = file.createNewFile();
+//            FileWriter writer = new FileWriter(file);
+//            writer.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return res;
     }
 
     public static void init(Context c){
@@ -23,7 +62,7 @@ public class Log {
             logger.d(tag,log);
         }
     }
-
+    
     public static Log getInstance(){
         if (instance == null){
             instance = new Log();
